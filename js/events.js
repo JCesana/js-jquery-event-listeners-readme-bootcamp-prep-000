711808a5334f5f92b5e3b0e6634ac61bb65e0a8c
Josh Cesana <joshuacesana@gmail.com>
2018-05-08 08:16:55 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,8 +14,8 @@ function frameIt() {
 
 function pressIt() {
   $('input').on('keydown', function(key) {
-    if(key.which == 71) {
-      alert("'g' was pressed");
+    if (key.which == 71) {
+      alert("g was pressed");
     }
   });
 }
